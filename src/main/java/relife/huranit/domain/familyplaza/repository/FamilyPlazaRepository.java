6bd8590c507f65745086e408bbaef0ff2de5a7ca
hysong4u <hayeon_song@naver.com>
2024-08-31 07:44:04 +0900

    [feat] 가족플라자 조회 엔티티 repository 구현

---
+++ src/main/java/relife/huranit/domain/familyplaza/repository/FamilyPlazaRepository.java
@@ -0,0 +1,7 @@
+package relife.huranit.domain.familyplaza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import relife.huranit.domain.familyplaza.domain.FamilyPlaza;
+
+public interface FamilyPlazaRepository extends JpaRepository<FamilyPlaza, Long> {
+}
