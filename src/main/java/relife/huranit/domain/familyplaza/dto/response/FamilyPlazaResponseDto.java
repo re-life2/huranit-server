8b08e9875976d76844eeb5a4d429cd5130fa4457
hysong4u <hayeon_song@naver.com>
2024-08-31 07:44:11 +0900

    [feat] 가족플라자 조회 엔티티 dto 구현

---
+++ src/main/java/relife/huranit/domain/familyplaza/dto/response/FamilyPlazaResponseDto.java
@@ -0,0 +1,24 @@
+package relife.huranit.domain.familyplaza.dto.response;
+
+import relife.huranit.domain.familyplaza.domain.FamilyPlaza;
+
+public record FamilyPlazaResponseDto(
+
+        Long familyPlazaId,
+        String name,
+        String imageUrl,
+        String description,
+        String age,
+        String applyMethod
+) {
+    public static FamilyPlazaResponseDto fromEntity(FamilyPlaza familyPlaza) {
+        return new FamilyPlazaResponseDto(
+                familyPlaza.getFamilyPlazaId(),
+                familyPlaza.getName(),
+                familyPlaza.getImageUrl(),
+                familyPlaza.getDescription(),
+                familyPlaza.getAge(),
+                familyPlaza.getApplyMethod()
+        );
+    }
+}
