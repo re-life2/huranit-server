3bb7cbdf9eac28c8f2b9bd524addfc164b9c9630
hysong4u <hayeon_song@naver.com>
2024-08-31 14:29:29 +0900

    [feat] 예약 엔티티에 예약 시작시간, 끝시간 추가

--- src/main/java/relife/huranit/domain/reservation/domain/Reservation.java
+++ src/main/java/relife/huranit/domain/reservation/domain/Reservation.java
@@ -1,15 +1,11 @@
 package relife.huranit.domain.reservation.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import relife.huranit.domain.center.domain.Center;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -49,10 +45,12 @@ public class Reservation {
     private LocalDate reserveDay;
 
     @Column
-    private LocalTime reserveTime;
+    private LocalTime reserveStartTime;
+
+    @Column LocalTime reserveEndTime;
 
     @Builder
-    public Reservation(Center center , String companyName , String departmentName, String name , String email , String companyCall , String userCall , String seat , LocalDate reserveDay , LocalTime reserveTime){
+    public Reservation(Center center, String companyName, String departmentName, String name, String email, String companyCall, String userCall, String seat, LocalDate reserveDay, LocalTime reserveStartTime, LocalTime reserveEndTime) {
         this.center = center;
         this.companyName = companyName;
         this.departmentName = departmentName;
@@ -62,6 +60,8 @@ public class Reservation {
         this.userCall = userCall;
         this.seat = seat;
         this.reserveDay = reserveDay;
-        this.reserveTime = reserveTime;
+        this.reserveStartTime = reserveStartTime;
+        this.reserveEndTime = reserveEndTime;
     }
+
 }
