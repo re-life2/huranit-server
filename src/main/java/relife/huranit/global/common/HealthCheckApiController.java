06254d4121a2b6e6d7a413c81616474c0966f1e6
hysong4u <hayeon_song@naver.com>
2024-09-05 23:55:53 +0900

    영상 업로드 테스트

--- src/main/java/relife/huranit/global/common/HealthCheckApiController.java
+++ src/main/java/relife/huranit/global/common/HealthCheckApiController.java
@@ -1,16 +1,33 @@
 package relife.huranit.global.common;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 @RestController
 public class HealthCheckApiController {
-    @RequestMapping("/")
-    public String HuranitServer() {
-        return "Hello Huranit Server!!";
-    }
 
+    @RequestMapping("/")
+    public ResponseEntity<byte[]> HuranitServer() throws IOException {
+        Resource resource = new ClassPathResource("static/example.mp4");
+        InputStream videoStream = resource.getInputStream();
+        byte[] videoBytes = StreamUtils.copyToByteArray(videoStream);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.valueOf("video/mp4"));
+        headers.setContentLength(videoBytes.length);
+
+        return new ResponseEntity<>(videoBytes, headers, HttpStatus.OK);
+    }
 
     @RequestMapping("/example")
     public ResponseEntity<SuccessResponse<?>> example() {
